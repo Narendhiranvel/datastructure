982019be0ddac8adbbed6f1fccf2498a49e7bd5f
Narendhiranvel <narendhiranvel@gmail.com>
2025-09-25 21:50:14 +0100

    Prefix sum

---
+++ src/Array/PrefixSum.java
@@ -0,0 +1,23 @@
+package Array;
+
+//Prefix Sum for range queries
+//Input: arr = [2,4,5,7,8], query = (1,3)
+//Output: 4+5+7 = 16
+
+public class PrefixSum {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, 7, 8};
+
+        System.out.println(query(arr, 1, 3)); // 16
+        System.out.println(query(arr, 0, 4)); // 26
+        System.out.println(query(arr, 2, 2)); // 5
+    }
+
+    private static int query(int[] arr, int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+}
