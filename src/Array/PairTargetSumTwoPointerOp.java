4752388a6e33e96df54f1c016a31027478705595
Narendhiranvel <narendhiranvel@gmail.com>
2025-09-29 21:59:24 +0100

    PairwithTargetSum Op commit 1

---
+++ src/Array/PairTargetSumTwoPointerOp.java
@@ -0,0 +1,37 @@
+package Array;
+
+//Pair with target sum in sorted array
+//Input: [1,2,3,4,6], target=6
+//Output: (2,4)
+
+public class PairTargetSumTwoPointerOp {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6};
+        int target = 6;
+        pairSum(arr, target);
+    }
+
+    private static void pairSum(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right){
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println("Pair found: (" + arr[left] + ", " + arr[right] + ")");
+                return;  // stop after finding the first pair
+            }
+
+            else if (sum < target) {
+                left++;   // need a bigger sum → move left pointer right
+            }
+            else {
+                right--;  // need a smaller sum → move right pointer left
+            }
+        }
+    }
+}
+
+// Time complexity O(n)
+// Space complexity O(1)
