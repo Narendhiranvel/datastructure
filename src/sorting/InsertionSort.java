885508f80ea8c0d63bf1a7dcfc43768bad88129f
Narendhiranvel <narendhiranvel@gmail.com>
2025-10-09 23:29:21 +0100

    InsertionSort commit 1

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+//Stability Sorting
+//Input: arr=[5,9,3,1,2,8,4,7,6]
+//Output: [1,2,3,4,5,6,7,8,9]
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 3, 1, 2, 8, 4, 7, 6};
+        insertionSorting(arr);
+    }
+
+    private static void insertionSorting(int[] arr) {
+        for(int i =0; i<arr.length; i++){
+            int temp = arr[i];
+            int j =i;
+            while(j>0 && arr[j-1] > temp){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+        for (int num : arr) {
+            System.out.printf(num + " ");
+        }
+    }
+}
+
+//Time Complexity: O(n^2)
+//Space Complexity: O(1)
