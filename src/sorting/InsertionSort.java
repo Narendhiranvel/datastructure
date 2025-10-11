8441276926e767ea45587c5559c9977dafd8bad9
Narendhiranvel <narendhiranvel@gmail.com>
2025-10-11 15:52:42 +0100

    Changed the method name in Insertion Sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
     }
 
     private static void insertionSorting(int[] arr) {
-        for(int i =0; i<arr.length; i++){
+        for(int i =1; i<arr.length; i++){
             int temp = arr[i];
             int j =i;
             while(j>0 && arr[j-1] > temp){
