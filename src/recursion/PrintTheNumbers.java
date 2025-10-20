1cc40834bfa763711934068bd5628e43a1460ac0
Narendhiranvel <narendhiranvel@gmail.com>
2025-10-20 22:16:03 +0100

    Recurssion 2

---
+++ src/recursion/PrintTheNumbers.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class PrintTheNumbers {
+    public static void main(String[] args) {
+        recursiveMethod(5);
+    }
+    private static void recursiveMethod(int n){
+        if(n<1){
+            System.out.println("n is less than 1");
+        } else{
+            recursiveMethod(n -1);
+            System.out.println(n);
+        }
+    }
+}
