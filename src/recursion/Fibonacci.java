22b154263c7e39d7f3f7aad9f77197bf3b4906f0
Narendhiranvel <narendhiranvel@gmail.com>
2025-10-25 16:54:37 +0100

    Fibonacci Recursion

---
+++ src/recursion/Fibonacci.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+
+        if (n < 0) {
+            System.out.print(-1);
+        } else {
+            for (int i = 0; i <= n; i++) {
+                System.out.print(fibonacci(i) + " ");
+            }
+        }
+    }
+
+    private static int fibonacci(int n) {
+        if (n == 0 || n == 1)
+            return n;
+
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+}
+
+//Time Complexity: O(2^n)
+//Space Complexity: O(n)
