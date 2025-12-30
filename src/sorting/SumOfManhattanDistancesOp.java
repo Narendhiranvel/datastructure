8f7abce455ae9ad9c2dd1091799ab37f27aa3d97
Narendhiranvel <narendhiranvel@gmail.com>
2025-12-30 20:04:41 +0000

    SumOfManhattanDistancesOp- Commit - 1.0

---
+++ src/sorting/SumOfManhattanDistancesOp.java
@@ -0,0 +1,47 @@
+package sorting;
+
+//Given n integer coordinates. The task is to find the sum of the Manhattan distance between all pairs of coordinates.
+//Manhattan Distance between (x1, y1) and (x2, y2) is: |x1 - x2| + |y1 - y2|
+//
+//Examples :
+//
+//Input : n = 4, p1 = { -1, 5 }, p2 = { 1, 6 }, p3 = { 3, 5 }, p4 = { 2, 3 }
+//Output : 22
+//Explanation :
+//Distance of { 1, 6 }, { 3, 5 }, { 2, 3 } from { -1, 5 } are 3, 4, 5 respectively. Therefore, sum = 3 + 4 + 5 = 12.
+//Distance of { 3, 5 }, { 2, 3 } from { 1, 6 } are 3, 4 respectively. Therefore, sum = 12 + 3 + 4 = 19
+//Distance of { 2, 3 } from { 3, 5 } is 3. Therefore, sum = 19 + 3 = 22.
+
+import java.util.Arrays;
+
+public class SumOfManhattanDistancesOp {
+
+    private static int distancesum(int[] arr, int n) {
+        Arrays.sort(arr);
+
+        int result = 0;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            result += (arr[i] * i - sum);
+            sum += arr[i];
+        }
+        return result;
+    }
+
+    static int totaldistancesum(int x[], int y[], int n) {
+        // Adding the distances along x and y axis.
+        return distancesum(x, n) + distancesum(y, n);
+    }
+
+    public static void main(String[] args) {
+        int x[] = {-1, 1, 3, 2};
+        int y[] = {5, 6, 5, 3};
+        int n = x.length;
+
+        System.out.println(totaldistancesum(x, y, n));
+    }
+}
+
+//Time Complexity: = O(n log n)
+//Space Complexity: 0(1)
