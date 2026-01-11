6832b8907025f491d0c3b25bd9cdb7e58f99114e
Narendhiranvel <narendhiranvel@gmail.com>
2026-01-11 17:46:23 +0000

    SumOfSubArrays Commit - 1.0

---
+++ src/contributionPattern/SumOfSubArrays.java
@@ -0,0 +1,28 @@
+package contributionPattern;
+
+public class SumOfSubArrays {
+
+    static class Solution {
+        public int subarraySum(int[] arr) {
+            int n = arr.length;
+            int sumOfArr = 0;
+
+            for (int i = 0; i < n; i++) {
+                sumOfArr += arr[i] * (i + 1) * (n - i);
+            }
+            return sumOfArr;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] arr = {1, 2, 3}; // sample input
+
+        int result = s.subarraySum(arr);
+        System.out.println("Sum of all subarrays = " + result);
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity : O(1)
