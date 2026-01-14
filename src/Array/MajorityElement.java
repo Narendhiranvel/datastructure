7b94228936c93c58b20cfc9c5c5b859f9a26d00c
Narendhiranvel <narendhiranvel@gmail.com>
2026-01-14 19:59:15 +0000

    MajorityElement Commit - 1.0

---
+++ src/Array/MajorityElement.java
@@ -0,0 +1,63 @@
+package Array;
+//https://leetcode.com/problems/majority-element-ii/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElement {
+
+    static class Solution {
+        public List<Integer> majorityElement(int[] nums) {
+            int n = nums.length;
+            int candidate1 = 0;
+            int candidate2 = 0;
+            int count1 = 0;
+            int count2 = 0;
+
+            // Phase 1: Find potential candidates
+            for (int num : nums) {
+                if (num == candidate1) {
+                    count1++;
+                } else if (num == candidate2) {
+                    count2++;
+                } else if (count1 == 0) {
+                    candidate1 = num;
+                    count1 = 1;
+                } else if (count2 == 0) {
+                    candidate2 = num;
+                    count2 = 1;
+                } else {
+                    count1--;
+                    count2--;
+                }
+            }
+
+            // Phase 2: Verify candidates
+            count1 = 0;
+            count2 = 0;
+            for (int num : nums) {
+                if (num == candidate1) count1++;
+                else if (num == candidate2) count2++;
+            }
+
+            List<Integer> result = new ArrayList<>();
+            if (count1 > n / 3) result.add(candidate1);
+            if (count2 > n / 3) result.add(candidate2);
+
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = {1, 1, 1, 3, 3, 2, 2, 2}; // sample input
+
+        List<Integer> result = s.majorityElement(nums);
+        System.out.println(result);
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity : O(1)
+
