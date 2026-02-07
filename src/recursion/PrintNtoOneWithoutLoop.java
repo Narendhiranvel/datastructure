cede19d79b346225b9ff4c19340f1815f16fdb25
Narendhiranvel <narendhiranvel@gmail.com>
2026-02-07 21:47:36 +0000

    PrintNtoOneWithoutLoop Commit - 1.0

---
+++ src/recursion/PrintNtoOneWithoutLoop.java
@@ -0,0 +1,29 @@
+package recursion;
+//https://www.geeksforgeeks.org/problems/print-n-to-1-without-loop/1
+
+public class PrintNtoOneWithoutLoop {
+
+    static class Solution {
+
+        void printNos(int N) {
+            // print current number
+            System.out.print(N + " ");
+            // recursive call until 1
+            if (N != 1) {
+                printNos(N - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int N = 10; // sample input
+        System.out.println("Printing numbers from " + N + " to 1:");
+        s.printNos(N);
+    }
+}
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
+
