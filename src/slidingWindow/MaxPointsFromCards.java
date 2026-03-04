96a75ec4bbdd292dcc9c479d56c63394add6e32b
Narendhiranvel <narendhiranvel@gmail.com>
2026-03-04 19:50:23 +0000

    MaxPointsFromCards- MYSOL 1.0

--- src/slidingWindow/MaxPointsFromCards.java
+++ src/slidingWindow/MaxPointsFromCards.java
@@ -5,29 +5,29 @@ public class MaxPointsFromCards {
 
     static class Solution {
         public int maxScore(int[] cardPoints, int k) {
-            int sum = 0;
-            int minContinuousBlock = Integer.MAX_VALUE;
+            int n = cardPoints.length;
+            int gap = n - k;
+            int totalPoints = 0;
 
-            // Total sum of array
-            for (int i = 0; i < cardPoints.length; i++) {
-                sum += cardPoints[i];
+            for (int i = 0; i < n; i++) {
+                totalPoints += cardPoints[i];
             }
-            int block = cardPoints.length - k;  // size of subarray to remove
-            int winSum = 0;
-            // Initial window
-            for (int i = 0; i < block; i++) {
-                winSum += cardPoints[i];
+            int gapTotal = 0;
+
+            for (int i = 0; i < gap; i++) {
+                gapTotal += cardPoints[i];
             }
 
-            minContinuousBlock = Math.min(minContinuousBlock, winSum);
-            int winStart = 0;
-            // Sliding window
-            for (int winEnd = block; winEnd < cardPoints.length; winEnd++) {
-                winSum += cardPoints[winEnd] - cardPoints[winStart];
-                winStart++;
-                minContinuousBlock = Math.min(minContinuousBlock, winSum);
+            int maxPoints = totalPoints - gapTotal;
+            int left = 0;
+
+            for (int right = gap; right < n; right++) {
+                gapTotal += cardPoints[right] - cardPoints[left];
+                maxPoints = Math.max(maxPoints, totalPoints - gapTotal);
+                left++;
             }
-            return sum - minContinuousBlock;
+
+            return maxPoints;
         }
     }
 
