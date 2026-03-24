3461a35bafb9112dbe8b0b55d50e8f5e5ea78dde
Narendhiranvel <narendhiranvel@gmail.com>
2026-03-24 07:21:13 +0000

    searchingProblems- comments

--- src/searchingProblems/FindPeekElementInArray.java
+++ src/searchingProblems/FindPeekElementInArray.java
@@ -1,15 +1,6 @@
 package searchingProblems;
 
-//A peak element is an element that is strictly greater than its neighbors.
-//Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
-//You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
-//You must write an algorithm that runs in O(log n) time.
-//
-//Example:
-//Input: nums = [1,2,3,1]
-//Output: 2
-//Explanation: 3 is a peak element and your function should return the index number 2.
-
+//https://leetcode.com/problems/find-peak-element/submissions/1957514449/
 public class FindPeekElementInArray {
 
     public static int findPeakElement(int[] nums) {
