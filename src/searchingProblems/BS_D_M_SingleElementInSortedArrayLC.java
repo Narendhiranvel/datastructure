d976c767e929a09ad5ccdabff83f7fd3a8afe8a5
Narendhiranvel <narendhiranvel@gmail.com>
2026-05-16 18:17:56 +0100

    BS_D_M_SingleElementInSortedArrayLC commit - new method

--- src/searchingProblems/BS_D_M_SingleElementInSortedArrayLC.java
+++ src/searchingProblems/BS_D_M_SingleElementInSortedArrayLC.java
@@ -4,32 +4,28 @@ package searchingProblems;
 //Input: nums = [1,1,2,3,3,4,4,8,8]
 //Output: 2
 
-public class BS_D_M_SingleNodeDuplicateLC {
+public class BS_D_M_SingleElementInSortedArrayLC {
 
     public static int singleNonDuplicate(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
+        int start = 0;
+        int end = nums.length - 1;
 
-        while (left < right) {
-            int mid = (left + right) / 2;
+        while (start < end) {
+            int middle = start + (end - start) / 2;
 
-            if (mid % 2 == 1) {
-                mid--;
-            }
-            if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
+            if ((middle % 2 == 0 && nums[middle] == nums[middle + 1]) || (middle % 2 != 0 && nums[middle] != nums[middle + 1])) {
+                start = middle + 1;
             } else {
-                right = mid;
+                end = middle;
             }
         }
-
-        return nums[left];
+        return nums[start];
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1,1,2,3,3,4,4,8,8};
+        int[] nums1 = {1, 1, 2, 3, 3, 4, 4, 8, 8};
         int result1 = singleNonDuplicate(nums1);
-        System.out.println("Input: [1,1,2,3,3]");
+        System.out.println("Input: [1,1,2,3,3,4,4,8,8]");
         System.out.println("Output: " + result1);
 
         int[] nums2 = {3, 3, 7, 7, 10, 11, 11};
@@ -39,5 +35,5 @@ public class BS_D_M_SingleNodeDuplicateLC {
     }
 }
 
-// Time Complexity: O(n)
- // Space Complexity: O(1)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
