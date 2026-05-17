0951c99b0c416f01fff70a15a5faa02c8f56e0bf
Narendhiranvel <narendhiranvel@gmail.com>
2026-05-17 14:45:36 +0100

    BS_F_M_FindMinimumInRotatedArraycommit - 3

--- src/searchingProblems/BS_F_M_FindMinimumInRotatedArray.java
+++ src/searchingProblems/BS_F_M_FindMinimumInRotatedArray.java
@@ -1,30 +1,29 @@
 package searchingProblems;
 //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
 public class BS_F_M_FindMinimumInRotatedArray {
 
     public int findMin(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
 
-        while(start < end){
-            int mid = start + (end - start)/2;
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
 
-            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
-                start++;
-                end--;
-            }
-
-            if(nums[mid] > nums[end]){
-                start = mid + 1;
+            if (nums[middle] < nums[end]) {
+                end = middle;
+            } else if (nums[middle] > nums[end]) {
+                start = middle + 1;
             } else {
-                end = mid;
+                end--;
             }
         }
         return nums[start];
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 1, 0, 1};
+        int[] nums = {3, 3, 1, 3};
 //     {2,0,1,1,1}
 //        1,1,1,0,1
         BS_F_M_FindMinimumInRotatedArray f = new BS_F_M_FindMinimumInRotatedArray();
