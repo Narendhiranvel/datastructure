18f97b9c9ff006da86af327e747583a5ec539a9d
Narendhiranvel <narendhiranvel@gmail.com>
2026-05-31 18:58:17 +0100

    EquilibriumIndexOp commit - added link

--- src/PrefixSuffix/EquilibriumIndexOp.java
+++ src/PrefixSuffix/EquilibriumIndexOp.java
@@ -1,35 +1,41 @@
 package PrefixSuffix;
-
+//https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/1
 //Equilibrium Index (left sum = right sum)
 //Input: [1,7,3,6,5,6]
 //Output: Index = 3
 
 public class EquilibriumIndexOp {
-    public static void main(String[] args) {
-        int[] arr = {1, 7, 3, 6, 5, 6};
-        int equilibriumIndex = -1;
-        int totalSum = 0;
-        int leftSum = 0;
 
-        for (int sum : arr) {
-            totalSum += sum;
-        }
+    static class Solution {
+        public static int findEquilibrium(int arr[]) {
+            int n = arr.length;
+            int arrTotal = 0;
 
-        System.out.println("Total sum Value of array " + totalSum);
-
-        for (int i = 0; i < arr.length; i++) {
-            int rightSum = totalSum - leftSum - arr[i];
-            if (leftSum == rightSum) {
-                equilibriumIndex = i;
-                break;
+            for (int sum : arr) {
+                arrTotal += sum;
             }
-            leftSum += arr[i];
+
+            int equilibriumPoint = -1;
+            int leftTotal = 0;
+
+            for (int i = 0; i < n; i++) {
+                leftTotal += arr[i];
+
+                if ((leftTotal - arr[i]) == (arrTotal - leftTotal)) {
+                    equilibriumPoint = i;
+                    break;
+                }
+            }
+            return equilibriumPoint;
         }
-        if (equilibriumIndex == -1) {
-            System.out.println("No equilibrium index found");
-        } else {
-            System.out.println("Equilibrium Index: " + equilibriumIndex);
-        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-7, 1, 5, 2, -4, 3, 0};
+
+        int result = Solution.findEquilibrium(arr);
+
+        System.out.println("Equilibrium Index: " + result);
     }
 }
 
