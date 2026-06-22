94033fdd28ed3c1e136d9453caad8876d1789372
Narendhiranvel <narendhiranvel@gmail.com>
2026-06-22 21:11:13 +0100

    Fibonacci - Commit 2.0

--- src/recursionAndBacktracking/Fibonacci.java
+++ src/recursionAndBacktracking/Fibonacci.java
@@ -14,10 +14,8 @@ public class Fibonacci {
     }
 
     private static int fibonacci(int n) {
-        if (n == 0 || n == 1)
-            return n;
-
-        return fibonacci(n - 2) + fibonacci(n - 1);
+        if(n == 0 || n == 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
 
