742b7529984a9c41f2bb9fb7259be1effded5bdc
Narendhiranvel <narendhiranvel@gmail.com>
2026-07-02 19:48:51 +0100

    RotateArrayKPositions commit 3

--- src/TwoPointerArray/RotateArrayKPositions.java
+++ src/TwoPointerArray/RotateArrayKPositions.java
@@ -14,28 +14,19 @@ public class RotateArrayKPositions {
             int n = nums.length;
             k = k % n;
 
-            // Step 1: reverse whole array
             reverse(nums, 0, n - 1);
-
-            // Step 2: reverse first k elements
             reverse(nums, 0, k - 1);
-
-            // Step 3: reverse remaining elements
             reverse(nums, k, n - 1);
         }
 
-        private void reverse(int[] nums, int i, int j) {
-            while (i < j) {
-                swap(nums, i, j);
-                i++;
-                j--;
+        private void reverse(int[] nums, int left, int right) {
+            while (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
             }
-        }
-
-        private void swap(int[] nums, int num1, int num2) {
-            int temp = nums[num1];
-            nums[num1] = nums[num2];
-            nums[num2] = temp;
         }
     }
 
